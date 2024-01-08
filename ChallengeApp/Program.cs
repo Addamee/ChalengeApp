1c82c3b718978b53226350d6ca12c2ec326fa9be
Adam Nuckowski <addbomer@gmail.com>
2024-01-08 19:23:12 +0100

    Zadanie dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Eva";
+string sex = "Kobieta";
+var age = 30;
+
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Eva" && age == 30)
+{
+    Console.WriteLine("Ewa, 30 lat");
+}
+else if (sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Meżczyzna");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Crtl + k + d = wyrównanie kodu
+// operatory relacyjne - if
+// == równa się
+// != różne
+// < , > , <= , >= 
+
+
+// oeratory logiczne
+// && i
+// || lub
+// ! negacja
+
+
+
+
+// zmenne testowe, sprawdź czy sie przyda
+//string name = "Adam";
+//string surname = "...";
+//string result = name + surname;
+//Console.Wrteline(result);
+//  /string result = name + myAge
+
+// bool isActive = true;   / false;
+// jak chcemy sprawdzic / var isValid = 5 > 6;
+// Console.Writeline(isValid);
